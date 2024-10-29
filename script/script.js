acadedc750af21391661d39b805c740e198dbb44
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-10-29 15:52:59 +0100

    add 3 module

--- script/script.js
+++ script/script.js
@@ -20,8 +20,10 @@ const nThree = "Fizz";
 const nFive = "Buzz";
 
 for(let i = 1; i < 101; i++){
-    if(i % 3 === 0 && i % 5 === 0){
+    if(i % 3 == 0 && i % 5 == 0){
         console.log(nThree+nFive);
+    }else if(i % 3 == 0){
+        console.log(nThree);
     }else{
         console.log(i);
     }
